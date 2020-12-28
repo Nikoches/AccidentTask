a18007a5c24c06687ba25be0d90c77826fb91f53
Nikoches <nikoches@gmail.com>
2020-12-28 22:06:06 +0300

    Initial commit

---
+++ src/main/java/main/WebConfig.java
@@ -0,0 +1,21 @@
+package main;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@ComponentScan("main")
+public class WebConfig {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("./WEB-INF/views/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+}
